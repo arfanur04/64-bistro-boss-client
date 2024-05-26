baf1cadc89ae51e29f978d8b0c4b20579e6d9a7e
arfanur04 <arfanur04@gmail.com>
2024-05-26 21:34:21 +0600

    65-2 Implement Cover With With Blur Parallax Using React Parallax

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import { Helmet } from "react-helmet-async";
 import { websiteTitle } from "../../../providers/AuthProvider";
+import Cover from "../../Shared/Cover/Cover";
+import menuBg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 
 const Menu = () => {
 	return (
@@ -7,6 +10,21 @@ const Menu = () => {
 			<Helmet>
 				<title>{websiteTitle} - Menu</title>
 			</Helmet>
+			<Cover
+				img={menuBg}
+				title={"Our Menu"}
+			></Cover>
+			<PopularMenu></PopularMenu>
+			<Cover
+				img={menuBg}
+				title={"Our Menu"}
+			></Cover>
+			<PopularMenu></PopularMenu>
+			<Cover
+				img={menuBg}
+				title={"Our Menu"}
+			></Cover>
+			<PopularMenu></PopularMenu>
 			<div>
 				<h2>This is Menu</h2>
 			</div>
