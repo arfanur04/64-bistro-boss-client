5765f766cef006e626b86353f84d80be3aefb2ee
arfanur04 <arfanur04@gmail.com>
2024-05-29 12:49:52 +0600

    66-3 Firebase Project Setup For Bistro Boss

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,12 +1,19 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { getAuth } from "firebase/auth";
+import { app } from "../firebase/firebase";
 
 export const AuthContext = createContext(null);
 export const websiteTitle = "Bistro Boss";
 export const apiURL = "http://localhost:5000";
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+	const [user, setUser] = useState(null);
+	const [loading, setLoading] = useState(true);
+
 	const authInfo = {
-		//
+		user,
+		loading,
 	};
 	return (
 		<AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
