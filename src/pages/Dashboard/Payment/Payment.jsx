f8f9b99f72f30eae7a7ecd07c4967ee191602ba9
arfanur04 <arfanur04@gmail.com>
2024-07-01 11:31:26 +0600

    70-2 Install Stripe, Stripe For React And Create Card Element

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,6 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import { websiteTitle } from "../../../utility/utility";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+// todo: add publishable key
+const stripePromise = loadStripe("");
 
 const Payment = () => {
 	return (
@@ -13,7 +18,11 @@ const Payment = () => {
 					heading={"Payment"}
 					subHeading={"Please pay to eat"}
 				/>
-				<h2 className="text-4xl">Teka o pakhi tumi uira uira aso</h2>
+				<div>
+					<Elements stripe={stripePromise}>
+						
+					</Elements>
+				</div>
 			</div>
 		</>
 	);
